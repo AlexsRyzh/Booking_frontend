8b045ca59271f7beb181a4dacb5ec198356fd00c
Alexs <alexsandr.ryzhkov03@mail.ru>
2023-04-23 18:28:16 +0300

    Адаптирован слайдер для мобильных устройств

--- src/components/CustomeSwipper/CustomSwipper.js
+++ src/components/CustomeSwipper/CustomSwipper.js
@@ -5,43 +5,65 @@ import styles from './cutomer_swipper.module.scss'
 import CountryCard from '../CountryCard/CountryCard';
 import SwipperButton from './SwipperButton/SwipperButton'
 import { Navigation } from 'swiper';
-
+import useWindowSize from '../../hooks/useWindowSize';
 
 const CustomSwipper = () => {
     const arr = new Array(10).fill(1)
 
+    const { width } = useWindowSize()
     const swiperRef = useRef();
 
 
     return (
         <div className={styles['container']}>
-            <div className={styles['btn_container']}>
-                <SwipperButton prev className={styles['btn']} onClick={() => swiperRef.current?.slidePrev()} />
-            </div>
+            {
+                width > 1100 &&
+                <div className={styles['btn_container']}>
+                    <SwipperButton prev className={styles['btn']} onClick={() => swiperRef.current?.slidePrev()} />
+                </div>
+            }
             <Swiper
-                slidesPerView={4}
-                spaceBetween={40}
+                slidesPerView={1}
                 modules={[Navigation]}
                 onBeforeInit={(swiper) => {
                     swiperRef.current = swiper;
                 }}
-                pagination={{
-                    clickable: true,
+                breakpoints={{
+                    720: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                    },
+                    1100: {
+                        slidesPerView: 3,
+                        spaceBetween: 40,
+                    },
+                    1360: {
+                        slidesPerView: 4,
+                        spaceBetween: 0,
+                    },
                 }}
 
                 className={styles['mySwiper']}
             >
                 {
                     arr.map(() => (
-                        <SwiperSlide>
+                        <SwiperSlide style={{
+                            width: 'fit-content',
+                            display: 'flex',
+                            justifyContent: 'center'
+                        }}>
                             <CountryCard />
                         </SwiperSlide>
                     ))
                 }
             </Swiper>
-            <div className={styles['btn_container']}>
-                <SwipperButton next className={styles['btn']} onClick={() => swiperRef.current?.slideNext()} />
-            </div>
+            {
+                width > 1100 &&
+                <div className={styles['btn_container']}>
+                    <SwipperButton next className={styles['btn']} onClick={() => swiperRef.current?.slideNext()} />
+                </div>
+            }
+
         </div>
     )
 }
