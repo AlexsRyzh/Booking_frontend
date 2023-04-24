6c6dd7781d4a3e6757721737c268d77225bc0c4e
AlexsRyzh <alexsandr.ryzhkov04@gmai.com>
2023-04-24 19:59:43 +0300

    Добавлена возможность перелистывать слайды в карусели

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,104 +1,72 @@
-import React, { useState, useEffect, cloneElement, Children } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import styles from './carousel.module.scss'
 import useWindowSize from '../../hooks/useWindowSize'
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+import { Autoplay } from "swiper";
+import "swiper/css/pagination";
 
-const TRANSITION_DURATION = 500
 
 
 
 const Carousel = ({ children, infinite, ...props }) => {
+    const arr = new Array(5).keys()
+    const pagination = {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '"></span>';
+        },
+    };
 
     const { width } = useWindowSize()
-    const [pages, setPages] = useState([])
-    const [clonesCount, setClonesCount] = useState({ head: 0, tail: 0 })
-    const [offset, setOffset] = useState(1)
-    const [transitionDuration, setTransitionDuration] = useState(0)
-    const [leftMargin, setLeftMargin] = useState(0)
-    const lines = new Array(children.length).fill(0)
 
+    const swiperRef = useRef(null)
+    const lines = new Array(4).fill(0)
+    const [offset, setOffset] = useState(0)
 
     useEffect(() => {
-        setLeftMargin((width - 1440) / 2)
-    }, [width])
-
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         setOffset(offset => offset + 1)
-    //     }, 5000)
-    //     console.log(1)
-    //     return () => {
-    //         console.log(2)
-    //         clearInterval(interval)
-    //     }
-    // }, [offset])
-
-    useEffect(() => {
-        if (infinite) {
-            setPages([
-                cloneElement(children[Children.count(children) - 1]), // tail: 1
-                ...children,
-                cloneElement(children[0]), // head: 1
-            ])
-            setClonesCount({ head: 1, tail: 1 })
-            return
-        }
-        setPages(children)
-    }, [children, infinite])
-
-    useEffect(() => {
-        if (transitionDuration === 0) {
-            setTimeout(() => {
-                setTransitionDuration(TRANSITION_DURATION)
-            }, TRANSITION_DURATION)
-        }
-    }, [transitionDuration])
-
-    useEffect(() => {
-        if (!infinite) return
-
-        // с элемента 0 (clone) -> к предпоследнему (реальный)
-        if (offset === pages.length - 1) {
-            setTimeout(() => {
-                setTransitionDuration(0)
-                setOffset(1)
-            }, TRANSITION_DURATION)
-            return
-        }
-        // с элемента n (clone) -> к элементу 1 (реальный)
-        if (offset === 0) {
-            setTimeout(() => {
-                setTransitionDuration(0)
-                setOffset(pages.length - 1)
-            }, TRANSITION_DURATION)
-            return
-        }
-    }, [offset, infinite, pages, clonesCount, width])
+        swiperRef.current.setTransition(500)
+    }, [])
 
     return (
-        <div className={styles['container']}>
-            <div
-                className={styles['slider_container']}
-                style={{
-                    transform: `translateX(-${offset * width}px)`,
-                    transitionDuration: `${transitionDuration}ms`,
-
-
+        <div className="App">
+            <Swiper
+                className="mySwiper"
+                onBeforeInit={(swiper) => {
+                    swiperRef.current = swiper;
                 }}
+                loop={true}
+                onSlideChange={() => {
+                    setOffset(swiperRef.current.realIndex)
+                }}
+                autoplay={{
+                    delay: 4000,
+                    disableOnInteraction: false,
+                }}
+                modules={[Autoplay]}
             >
-                {pages}
-            </div>
-            <div className={styles['progress']} style={{
-                left: `${60 + (leftMargin > 0 ? leftMargin : 0)}px`
-            }}>
-                {lines.map((value, index) => (
-                    <div
-                        className={styles['line'] + " " + ((offset === index + 1) ? styles['active'] : "")}
-                        onClick={() => {
-                            setOffset(index + 1)
-                        }}
-                        key={index}></div>
-                ))}
-            </div>
+                {
+                    children.map((page, index) => (
+                        <SwiperSlide className="slide" key={index}>
+                            {page}
+                        </SwiperSlide>
+                    ))
+                }
+                <div className={styles['progress']} style={{
+                    left: `${((width - 1440) / 2 > 0 ? (width - 1440) / 2 + 60 : 60)}px`
+                }}>
+                    {lines.map((value, index) => (
+                        <div
+                            className={styles['line'] + " " + ((offset === index) ? styles['active'] : "")}
+                            onClick={() => {
+                                setOffset(index)
+                                swiperRef.current.slideTo(index)
+                            }}
+                            key={index}></div>
+                    ))}
+                </div>
+            </Swiper>
         </div>
     )
 }
